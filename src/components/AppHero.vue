70e65931144c55a3f3c3aeb609083013db227175
Francesco Rocco <francescorocco97@gmail.com>
2023-03-24 16:18:41 +0100

    Esercizio con bonus completo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="jumbotron">
+
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+   .jumbotron {
+    width: 100%;
+    height: 450px;
+    background-image: url(https://images7.alphacoders.com/330/330058.jpeg);
+    background-position: center;
+    background-size: cover;
+    margin: 0 auto;
+    position: absolute;
+    top: 300px;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    h1{
+        font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        margin: 100px 0;
+    }
+</style>
